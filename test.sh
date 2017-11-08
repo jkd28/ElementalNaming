1e5b4124d6111e0ebd89c7241034389244b5d28c
JD <jkd28@pitt.edu>
2017-11-08 01:33:59 -0500

    Upload unit testing dependencies

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo "Compiling Tests..."
-if javac -cp .:hamcrest-core-1.3.jar:junit-4.12.jar *.java; then
+if javac -cp .:jars/hamcrest-core-1.3.jar:jars/junit-4.12.jar *.java; then
     echo "Executing Tests..."
-    java -cp .:hamcrest-core-1.3.jar:junit-4.12.jar TestRunner
+    java -cp .:jars/hamcrest-core-1.3.jar:jars/junit-4.12.jar TestRunner
 else
     echo "TESTS FAILED TO COMPILE, NO TESTS EXECUTED"
 fi
